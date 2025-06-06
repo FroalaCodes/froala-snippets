7fb23d3cec62232487391b55ccf0046d81118ce5
Carl <104518119+FroalaCodes@users.noreply.github.com>
2025-06-06 17:21:29 +0800

    Update setup_project.sh

---
+++ content/blogs/scalable-websites-with-apis/setup_project.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script to initialize the Node.js project and install dependencies
+
+echo "Initializing Node.js project..."
+npm init -y
+
+echo "Installing Axios..."
+npm install axios
+
+echo "Setup complete. You can now create your app.js file."
